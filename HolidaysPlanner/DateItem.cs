ed4ca2803912311683b87c7a246043e0e880dd56
MM <mmur.developer@gmail.com>
2021-11-19 15:52:51 +0100

    init

---
+++ HolidaysPlanner/DateItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HolidaysPlanner
+{
+    public class DateItem
+    {
+        public DateItem(DateTime dateStart, DateTime dateEnd)
+        {
+            this.dateStart = dateStart;
+            this.dateEnd = dateEnd;
+        }
+
+        DateTime dateStart;
+        public DateTime DateStart
+        {
+            get => dateStart; set => dateStart = value;
+        }
+
+        DateTime dateEnd;
+        public DateTime DateEnd { get => dateEnd; set => dateEnd = value; }
+
+        private List<DateTime> GetDays(DateTime start, DateTime end)
+        {
+            List<DateTime> dateList = new List<DateTime>();
+            for (DateTime i = dateStart; i <= dateEnd; i.AddDays(1))
+            {
+                dateList.Add(i);
+                i = i.AddDays(1);
+            }
+            return dateList;
+        }
+
+        public int GetMyHolidaysDaysWithoutWeekendsAndCelebrations(List<DateTime> freeDays)
+        {
+            var days = GetDays(dateStart, dateEnd);
+            var weekendsAndCelebritiesCount = days.Where(d => d.DayOfWeek == DayOfWeek.Saturday || d.DayOfWeek == DayOfWeek.Sunday || freeDays.Contains(d)).Count();
+            //var dayBeforeStart = dateStart.AddDays(-1);
+            //var dayAfterStart = dateStart.AddDays(-1);
+            return days.Count - weekendsAndCelebritiesCount;
+        }
+        public int GetDaysCount(List<DateTime> freeDays)
+        {
+            return GetDays(dateStart, dateEnd).Count;
+        }
+    }
+}
---
+++ HolidaysPlanner/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace HolidaysPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bin/debug
+            string workingDirectory = Environment.CurrentDirectory;
+            //bin/
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+
+            string projectDirectory2 = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+
+            string workFreeDaysCsvDirectory = $"{projectDirectory2}\\Data\\WorkFreeDays.csv";
+            string planningDaysCsvDirectory = $"{projectDirectory2}\\Data\\PlanningDays.csv";
+
+            string dateFormat = "dd.MM.yyyy";
+            var provider = new CultureInfo("pl-PL");
+
+            List<DateTime> workFreeDays = new List<DateTime>();
+            using (var reader = new StreamReader(workFreeDaysCsvDirectory))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var date = DateTime.ParseExact(line, dateFormat, provider);
+                    workFreeDays.Add(date);
+                }
+            }
+
+            List<DateItem> dateItems = new List<DateItem>();
+            using (var reader = new StreamReader(planningDaysCsvDirectory))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    List<string> splitLine = line.Split(",").ToList();
+                    var dateStart = DateTime.ParseExact(splitLine[0].ToString(), dateFormat, provider);
+                    var dateEnd = DateTime.ParseExact(splitLine[1].ToString(), dateFormat, provider);
+                    dateItems.Add(new DateItem(dateStart, dateEnd));
+                }
+            }
+            var sumWithoutWeekendAndCelebrations = 0;
+            foreach(var item in dateItems)
+            {
+                sumWithoutWeekendAndCelebrations+= item.GetMyHolidaysDaysWithoutWeekendsAndCelebrations(workFreeDays);
+            }
+            /*
+            var sumAll = 0;
+            foreach (var item in dateItems)
+            {
+                sumWithoutWeekendAndCelebrations += item.Get
+            }
+            */
+            Console.WriteLine($"Ilość dni wolnych nie licząc świąt i weekendów: {sumWithoutWeekendAndCelebrations}");
+            //Console.WriteLine($"Ilość dni wolnych licząc święta i weekendy: {sum}");
+        }       
+    }
+}
